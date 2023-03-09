d5425beab0e9448d9c4d933e1f94870c079fe290
Josh C <41541186+open-analysis@users.noreply.github.com>
2023-03-08 19:53:40 -0600

    Updated functionality
    - Expanded REST API to include put & delete
    - Added some functionality to request (print the request's json data)

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 queue = []
 
+# Return the queue of commands to the PC
 @app.get("/queue")
 def getQueue():
     global queue 
@@ -11,12 +12,40 @@ def getQueue():
     queue.clear()
     return json_queue
 
+# Add/Update/Delete programs
 @app.post("/programs")
 def add_program():
     if request.is_json:
-        pass
+        tmp_progs = request.get_json()
+        print(f"Adding {tmp_progs}")
 
-@app.post("/devices")
-def add_program():
+@app.put("/programs")
+def update_program():
     if request.is_json:
-        pass
+        tmp_progs = request.get_json()
+        print(f"Updating {tmp_progs}")
+
+@app.delete("/programs")
+def del_program():
+    if request.is_json:
+        tmp_progs = request.get_json()
+        print(f"Deleting {tmp_progs}")
+
+# Add/Update/Delete devices
+@app.post("/devices")
+def add_device():
+    if request.is_json:
+        tmp_devs = request.get_json()
+        print(f"Adding {tmp_devs}")
+
+@app.put("devices")
+def update_device():
+    if request.is_json:
+        tmp_devs = request.get_json()
+        print(f"Updating {tmp_devs}")
+
+@app.delete("/devices")
+def remove_device():
+    if request.is_json:
+        tmp_devs = request.get_json()
+        print(f"Deleting {tmp_devs}")
